9b3e39285735b30dd26a5d08c0c18b9131d1f4a0
Dawid Mostowski <dawid.mostowski06@proton.me>
2025-05-12 11:07:36 +0200

    update

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -26,10 +26,10 @@ SECRET_KEY = 'django-insecure-^k8^nn0b2a#@*fc14^_9z*dwo1nynl%vx_kg6am$#1xwa0py8m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'brave-beers-cheer.loca.lt']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'warm-suits-change.loca.lt']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://brave-beers-cheer.loca.lt/',
+    'https://warm-suits-change.loca.lt/',
 ]
 
 # Application definition
