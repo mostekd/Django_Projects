2380a71ea2df887d0a7d0fd65430f982db8c0067
Dawid Mostowski <dawid.mostowski06@proton.me>
2025-05-14 10:46:57 +0200

    update

--- todo/config/urls.py
+++ todo/config/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path("users/", include("todo.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    # ...
+    
     # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
--- todo/todo/users/views.py
+++ todo/todo/users/views.py
@@ -44,3 +44,5 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 
 user_redirect_view = UserRedirectView.as_view()
+
+
