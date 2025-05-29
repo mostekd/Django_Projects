516f4c6150775843f77a3c6769b655620d603d59
Dawid Mostowski <dawid.mostowski06@proton.me>
2025-05-29 10:18:03 +0200

    update

--- datacollector/datacollector/users/views.py
+++ datacollector/datacollector/users/views.py
@@ -74,10 +74,13 @@ class UserSubmissionViewSet(viewsets.ModelViewSet):
         name = self.request.query_params.get('name')
         city = self.request.query_params.get('city')
         year = self.request.query_params.get('year')
+        email = self.request.query_params.get('email') 
         if name:
             queryset = queryset.filter(name__icontains=name)
         if city:
             queryset = queryset.filter(city__iexact=city)
         if year:
             queryset = queryset.filter(birthdate__year=year)
+        if email:
+            queryset = queryset.filter(email__icontains=email)
         return queryset
